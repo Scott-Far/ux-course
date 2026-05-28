9f75b9d83f9d27cff5be96389ec580e98b03ea6a
Scott-Far <faranellos@gmail.com>
2026-05-28 19:40:29 -0400

    Add files via upload

--- airtable.js
+++ airtable.js
@@ -1,7 +1,15 @@
-var SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbxCE8NeQCKFThpxwmhzUvFUGjLOjRmDUMwiTc92vQE5tK8JzT15QoCgQOlEy_bpD_hQ/exec';
+var FORM_ACTION = 'https://docs.google.com/forms/d/e/1FAIpQLSf_adIaBigbeW8C2lC5o-9txs2sCztxtdPFgLcr9hutBDEh0Q/formResponse';
+
+var FIELD_IDS = {
+  studentName: 'entry.2108948697',
+  email:       'entry.1519332547',
+  lesson:      'entry.845222426',
+  activity:    'entry.1119830879',
+  response:    'entry.751849130'
+};
 
 function requireStudent() {
-  var name = localStorage.getItem('ux_student_name');
+  var name  = localStorage.getItem('ux_student_name');
   var email = localStorage.getItem('ux_student_email');
   if (!name || !email) {
     window.location.href = 'welcome.html';
@@ -11,15 +19,18 @@ function requireStudent() {
 
 function submitToAirtable(lesson, activity, response) {
   var student = requireStudent();
-  var params = new URLSearchParams({
-    studentName: student.name,
-    email: student.email,
-    lesson: lesson,
-    activity: activity,
-    response: response
+  var body = new FormData();
+  body.append(FIELD_IDS.studentName, student.name);
+  body.append(FIELD_IDS.email,       student.email);
+  body.append(FIELD_IDS.lesson,      lesson);
+  body.append(FIELD_IDS.activity,    activity);
+  body.append(FIELD_IDS.response,    response);
+
+  return fetch(FORM_ACTION, {
+    method: 'POST',
+    mode:   'no-cors',
+    body:   body
   });
-  var url = SUBMIT_URL + '?' + params.toString();
-  return fetch(url, { method: 'GET', mode: 'no-cors' });
 }
 
 function saveActivity(btn, lesson, activity, textareaId) {
